9b00c90894b2683e2877f5edfc933402990fa2f6
SreerekhaKV <sreerekha@cookee.io>
2025-08-19 12:21:24 +0530

    Refactor PickupAndDeliveryOverview widget to use a dynamic card configuration; improve layout and styling

--- lib/src/presentation/views/home/widgets/pickup_and_delivery_overview/pickup_and_delivery_overview.dart
+++ lib/src/presentation/views/home/widgets/pickup_and_delivery_overview/pickup_and_delivery_overview.dart
@@ -4,101 +4,137 @@ import 'package:spinners_driver/app/theme/app_colors.dart';
 import 'package:spinners_driver/app/theme/app_typography.dart';
 import 'package:spinners_driver/src/presentation/constants/app_images.dart';
 import 'package:the_responsive_builder/the_responsive_builder.dart';
-
 class PickupAndDeliveryOverview extends StatelessWidget {
   const PickupAndDeliveryOverview({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: 16.dp,
-      ),
+      margin: EdgeInsets.symmetric(horizontal: 16.dp),
       width: 100.w,
       height: 27.h,
       child: Stack(
         clipBehavior: Clip.none,
         alignment: AlignmentDirectional.bottomCenter,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(bottom: 19.h),
-            child: _pickupRemainingCard(),
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 12.h),
-            child: _myDeliveriesLeftCard(),
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 5.h),
-            child: _completedPickupCard(),
-          ),
-          _completedDeliveryCard(),
-        ],
+        children: _buildOverviewCards(),
       ),
     );
   }
 
-  Widget _completedDeliveryCard() {
-    return Container(
-      width: 100.w,
-      height: 48.dp,
-      decoration: const BoxDecoration(image: DecorationImage(image: AssetImage(AppImages.completedDeliveryCard), fit: BoxFit.fill)),
-      child: _overviewContent(
+  List<Widget> _buildOverviewCards() {
+    final cards = _getCardConfigurations();
+    return cards.map((config) => Padding(
+      padding: EdgeInsets.only(bottom: config.bottomPadding),
+      child: _buildOverviewCard(config),
+    )).toList();
+  }
+
+  List<_CardConfig> _getCardConfigurations() {
+    return [
+      _CardConfig(
+        bottomPadding: 19.h,
+        height: 62.dp,
+        backgroundImage: AppImages.pickupRemainingCard,
+        icon: AppImages.remainingPickupIcon,
+        label: 'Pickup Remaining',
+        value: '03',
+      ),
+      _CardConfig(
+        bottomPadding: 12.h,
+        height: 62.dp,
+        backgroundImage: AppImages.deliveriesLeftCard,
+        icon: AppImages.deliveriesLeftIcon,
+        label: 'My Deliveries Left',
+        value: '02',
+      ),
+      _CardConfig(
+        bottomPadding: 5.h,
+        height: 62.dp,
+        backgroundImage: AppImages.completedPickupCard,
+        icon: AppImages.checkIcon,
+        label: 'Completed Pickups',
+        value: '10',
+      ),
+      _CardConfig(
+        bottomPadding: 0.h,
+        height: 48.dp,
+        backgroundImage: AppImages.completedDeliveryCard,
         icon: AppImages.completedDeliveriesIcon,
         label: 'Completed Deliveries',
-        value: '5',
+        value: '08',
       ),
-    );
+    ];
   }
 
-  Widget _completedPickupCard() {
+  Widget _buildOverviewCard(_CardConfig config) {
     return Container(
       width: 100.w,
-      height: 62.dp,
-      decoration: const BoxDecoration(image: DecorationImage(image: AssetImage(AppImages.completedPickupCard), fit: BoxFit.fill)),
-       child: _overviewContent(
-        icon: AppImages.checkIcon,
-        label: 'Completed Deliveries',
-        value: '5',
+      height: config.height,
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 16.dp),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(config.backgroundImage),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: _buildCardContent(
+        icon: config.icon,
+        label: config.label,
+        value: config.value,
       ),
     );
   }
 
-  Widget _myDeliveriesLeftCard() {
-    return Container(
-      width: 100.w,
-      height: 62.dp,
-      decoration: const BoxDecoration(image: DecorationImage(image: AssetImage(AppImages.deliveriesLeftCard), fit: BoxFit.fill)),
-       child: _overviewContent(
-        icon: AppImages.deliveriesLeftIcon,
-        label: 'Completed Deliveries',
-        value: '5',
-      ),
-    );
-  }
-
-  Widget _pickupRemainingCard() {
-    return Container(
-      width: 100.w,
-      height: 62.dp,
-      decoration: const BoxDecoration(image: DecorationImage(image: AssetImage(AppImages.pickupRemainingCard), fit: BoxFit.fill)),
-       child: _overviewContent(
-        icon: AppImages.remainingPickupIcon,
-        label: 'Completed Deliveries',
-        value: '5',
-      ),
-    );
-  }
-
-  Widget _overviewContent({required String icon,required String label, required String value}) {
+  Widget _buildCardContent({
+    required String icon,
+    required String label,
+    required String value,
+  }) {
     return Row(
       children: [
-       Image.asset(icon, width: 24.dp, height: 24.dp,color: AppColors.white,),
-       Gap(12.dp),
-       Text(label,style: AppTypography.sfProRoundedSemiBold.copyWith(fontSize: 12.sp),),
-       Spacer(),
-       Text(value,style: AppTypography.sfProRoundedBold.copyWith(fontSize: 20.sp),),
+        Image.asset(
+          icon,
+          width: 24.dp,
+          height: 24.dp,
+          color: AppColors.white,
+        ),
+        Gap(12.dp),
+        Expanded(
+          child: Text(
+            label,
+            style: AppTypography.sfProRoundedSemiBold.copyWith(
+              fontSize: 12.sp,
+              color: AppColors.white.withValues(alpha: 0.5),
+            ),
+          ),
+        ),
+        Text(
+          value,
+          style: AppTypography.sfProRoundedBold.copyWith(
+            fontSize: 20.sp,
+            color: AppColors.white,
+          ),
+        ),
       ],
     );
   }
+}
+
+class _CardConfig {
+  final double bottomPadding;
+  final double height;
+  final String backgroundImage;
+  final String icon;
+  final String label;
+  final String value;
+
+  const _CardConfig({
+    required this.bottomPadding,
+    required this.height,
+    required this.backgroundImage,
+    required this.icon,
+    required this.label,
+    required this.value,
+  });
 }
