d79bfa558a819a3e01acbfe0f0b59626f365fd32
SreerekhaKV <sreerekha@cookee.io>
2025-09-01 15:37:49 +0530

    remove amount round off

--- lib/src/presentation/views/delivery_order_detail/delivery_order_detail_screen.dart
+++ lib/src/presentation/views/delivery_order_detail/delivery_order_detail_screen.dart
@@ -392,6 +392,7 @@ class _DeliveryOrderDetailScreenState extends State<DeliveryOrderDetailScreen> {
                       child: DeliveryConfirmBottomsheet(
                         orderId: orderId,
                         totalCollected: totalAmount,
+                        orderRefId: widget.refId,
                       ),
                     ),
                   );
@@ -414,7 +415,7 @@ class _DeliveryOrderDetailScreenState extends State<DeliveryOrderDetailScreen> {
                     ),
                     Gap(6.dp),
                     Text(
-                      'AED ${totalAmount.toStringAsFixed(0)}',
+                      'AED $totalAmount',
                       style: AppTypography.sfProRoundedSemiBold.copyWith(
                         fontSize: 12.dp,
                         color: AppColors.white,
