8b06511849416e736c645db036460ddb3100421f
SasthaSankar-CJ <sastha.cookee@gmail.com>
2025-11-26 11:50:07 +0530

    production url fix

--- lib/app/services/api_services/environment/config.dart
+++ lib/app/services/api_services/environment/config.dart
@@ -1,6 +1,6 @@
 class ApiUrls {
   static const String qaUrl = 'http://192.168.29.250:3001';
   static const String stagingUrl = 'http://3.29.254.53:3000';
-  static const String productionUrl = 'http://3.29.245.156:3000';
+  static const String productionUrl = 'https://api.spinnerslaundry.co';
   static const String prefix = '/driver';
 }
